90f88bfe78e737c71781f3d235055ab1ef0eaa22
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-17 20:17:25 +0530

    added login styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,11 +7,48 @@ const Login = (props) => {
                 <a href="/">
                    <img src="/images/login-logo.svg" alt="login logo"/>
                 </a>
+                <div>
+                    <Join>Join now</Join>
+                </div>
             </Nav>
         </Container>
     );
 };
 
-const Container = styled.div``;
-const Nav = styled.div``;
+const Container = styled.div`
+  padding:0px;
+`;
+const Nav = styled.nav`
+   max-width:1128px;
+   margin:auto;
+   padding: 12px 0 16px;
+   display: flex;
+   align-items: center;
+   position: relative;
+   justify-content: space-between;
+   flex-wrap: nowrap;
+
+
+   & > a {
+       width: 135px;
+       height:34px;
+       @media (max-width: 768px) {
+          padding: 0  5px;
+       }
+   }
+`;
+
+const Join = styled.a`
+    font-size: 19px;
+    padding: 10px 12px;
+    text-decoration: none;
+    border-radius: 4px;
+    color: rgba(0,0,0,0.6);
+    margin-right: 12px;
+    &:hover{
+        background-color:rgba(0,0,0,0.08);
+        color: rgba(0,0,0,0.9);
+        text-decoration: none;
+    }
+`;
 export default Login;
