b0ff53bce9a2abf2dce60852c6163397b740ae72
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-19 23:46:55 +0530

    added Home component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@
 import './App.css';
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
 import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login/>}/>
+          <Route path='/' element={<Login />} />
+          <Route path='/home' element={<Home/>} />
         </Routes>
+
       </BrowserRouter>
       
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+import { Styled } from "styled-components";
+
+const Header = () => {
+    return <div>Header</div>;
+};
+export default Header;
