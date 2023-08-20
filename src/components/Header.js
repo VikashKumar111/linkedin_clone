0116d73095536ad6e44bbc5d2049524854116ffe
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-20 23:56:59 +0530

    added signout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,6 +55,9 @@ const Header = () => {
                                 <span>Me</span>
                                 <img src="/images/down-icon.svg"  alt="down-icon"/>
                             </a>
+                            <SignOut>
+                                <a>Sign Out</a>
+                            </SignOut>
                         </User>
                         <Work>
                             <a>
@@ -203,6 +206,19 @@ const NavList = styled.li`
  
 `;
 
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px; 
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
+`;
+
 const User = styled(NavList)`
   a > img {
     width: 24px;
@@ -214,8 +230,19 @@ const User = styled(NavList)`
     display: flex;
     align-items: center;
   }
+
+  &:hover {
+    ${SignOut}{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+  }
 `;
+
 const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
+
+
 export default Header;
