51be1a6f0bdc5d267021dba00d009e7b811cb51f
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-20 23:05:32 +0530

    added the activation functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
                 </Search>
                 <Nav>
                     <NavListWrap>
-                        <NavList>
+                        <NavList className="active">
                             <a>
                                 <img src="/images/nav-home.svg" alt="nav-home" />
                                 <span>Home</span>
@@ -135,6 +135,22 @@ const NavListWrap = styled.ul`
  display: flex;
  flex-wrap: nowrap;
  list-style-type: none;
+
+ .active {
+    span:after {
+        content: " ";
+        transform: scaleX(1);
+        border-bottom: 2px solid var(--white, #fff);
+        bottom: 0;
+        left: 0;
+        position: absolute;
+        transition: transform 0.2s ease-in-out;
+        width: 100%;
+        border-color: rgba(0, 0, 0, 0.9);
+
+
+    }
+ }
 `;
 
 const NavList = styled.li`
