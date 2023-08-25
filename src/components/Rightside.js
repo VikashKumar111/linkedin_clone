cd1746eac542d5f2572695e7b00a1b953367a2e2
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-25 13:05:34 +0530

    added feedlist to rightside

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -8,6 +8,26 @@ const Rightside = () => {
           <h2>Add to your feed</h2>
           <img src="/images/feed-icon.svg" alt="" />
         </Title>
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FeedList>
       </FollowCard>
     </Container>
   );
@@ -37,4 +57,37 @@ const Title = styled.div`
   width: 100%;
   color: rgba(0, 0, 0, 0.6);
 `;
+
+const FeedList = styled.ul`
+  margin-top: 16px;
+  li {
+    display: flex;
+    align-items: center;
+    margin: 12px;
+    position: relative;
+    font-size: 14px;
+    & > div {
+      display: flex;
+      flex-direction: column;
+    }
+    button {
+      background-color: transparent;
+      color: rgba(0, 0, 0, 0.6);
+      box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      align-items: center;
+      border-radius: 15px;
+      box-sizing: border-box;
+      font-weight: 600;
+      display: inline-flex;
+      justify-content: center;
+      max-height: 32px;
+      max-width: 480px;
+      text-align: center;
+      outline: none;
+    }
+  }
+`;
+
+const Avatar = styled.div``;
 export default Rightside;
