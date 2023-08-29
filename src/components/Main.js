f19cb25029a3b4495e75a0a242fdc49a9f3fd9b3
Vikash Kumar <vikasthakur9310@gmail.com>
2023-08-30 00:46:23 +0530

    finished article component

--- src/components/Main.js
+++ src/components/Main.js
@@ -39,6 +39,9 @@ const Main = () => {
                 <span>Date</span>
               </div>
             </a>
+            <button>
+              <img src="/images/ellipsis.svg" alt="" />
+            </button>
           </SharedArticle>
         </Article>
       </div>
@@ -126,5 +129,55 @@ const Article = styled(CommonCard)`
   padding: 0;
   margin: 0 0 8px;
   overflow: visible;
+`;
+
+const SharedArticle = styled.div`
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+    img {
+      width: 30px;
+    }
+  }
 `;
 export default Main;
