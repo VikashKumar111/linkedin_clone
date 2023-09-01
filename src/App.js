1900269d49c04bc8dfc79b94d765f43d687778d7
Vikash Kumar <vikasthakur9310@gmail.com>
2023-09-01 23:51:56 +0530

    added geuUserAuth action

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import "./App.css";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
-import './App.css';
-import { BrowserRouter,Routes,Route} from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
-import Header from './components/Header';
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
 
-
-function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<><Header/><Home/></>} />
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
         </Routes>
-
       </BrowserRouter>
-      
     </div>
   );
 }
