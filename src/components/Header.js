0e994c270873da25a2a4749ec98bb01026a1bea2
Vikash Kumar <vikasthakur9310@gmail.com>
2023-09-02 11:24:53 +0530

    add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { connect } from "react-redux";
 import styled from "styled-components";
 
 const Header = (props) => {
@@ -51,8 +52,8 @@ const Header = (props) => {
             </NavList>
             <User>
               <a>
-                {props.user && props.user.photoUrl ? (
-                  <img src={props.user.photoUrl} alt="" />
+                {props.user && props.user.photoURL ? (
+                  <img src={props.user.photoURL} alt="" />
                 ) : (
                   <img src="/images/user.svg" alt="user" />
                 )}
@@ -248,4 +249,11 @@ const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.userState.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({});
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
